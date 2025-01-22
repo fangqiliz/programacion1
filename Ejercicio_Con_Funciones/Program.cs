e33ed513b4773b1d28fa15e2e9c0b50aee85ff5c
fangqiliz <fangqili5@gmail.com>
2025-01-22 19:23:05 -0400

    Add files via upload

---
+++ Ejercicio_Con_Funciones/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//  determinar si un año es bisiesto.
+
+using System;
+
+class Program
+{
+
+    static void Main()
+    {
+
+        Console.WriteLine("Introduce el año: ");
+        int year = int.Parse(Console.ReadLine());
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+        {
+            Console.WriteLine("El año es bisiesto.", year);
+        }
+        else
+        {
+            Console.WriteLine("El año no es bisiesto.", year);
+        }
+        Console.ReadKey();
+
+
+    }
+}
+
+
